48920499531d236e8466f7193c353bbdfcc0cfd3
shrawan dhawale <shrawan.dhawale@gmail.com>
2026-01-12 14:40:08 +0530

    Remove legacy root MarioGame source; keep game/MarioGame

--- src/main/java/com/in28minutes/learn_spring_framework/GameRunner.java
+++ src/main/java/com/in28minutes/learn_spring_framework/GameRunner.java
@@ -1,7 +1,5 @@
 package com.in28minutes.learn_spring_framework;
 
-import com.in28minutes.learn_spring_framework.game.MarioGame;
-
 public class GameRunner {
     MarioGame game;
 
--- src/main/java/com/in28minutes/learn_spring_framework/MarioGame.java
+++
@@ -1,6 +0,0 @@
-package com.in28minutes.learn_spring_framework;
-
-// Legacy/misplaced file adjusted so compiler sees only the correct `MarioGame` in the `game` package.
-class MarioGameLegacy {
-
-}
