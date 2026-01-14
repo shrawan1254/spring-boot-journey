bd29fdefbc36a35e80a33d1c14d75fa55720cedf
shrawan dhawale <shrawan.dhawale@gmail.com>
2026-01-14 15:31:36 +0530

    played more with Bean and add some more methods  Beans in spring

--- src/main/java/com/in28minutes/learn_spring_framework/HelloWorlfConfiguration.java
+++ src/main/java/com/in28minutes/learn_spring_framework/HelloWorlfConfiguration.java
@@ -1,7 +1,13 @@
 package com.in28minutes.learn_spring_framework;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+
+record Person(String name, int age, Address address){};
+
+record Address(String street, String city){ };
 
 @Configuration
 public class HelloWorlfConfiguration {
@@ -9,6 +15,46 @@ public class HelloWorlfConfiguration {
     @Bean
     public String name(){
         return "shrawan";
+    }
+
+    @Bean
+    public int age(){
+        return 21;
+    }
+
+    @Bean
+    public Person person(){
+        return new Person(name(), age(), address());
+    }
+
+    @Bean
+    public Person person2MethodCall(){
+        return new Person(name().toUpperCase(), age() + 1, address2());
+    }
+
+    @Bean
+    public Person person3Parameter(String name, int age, @Qualifier("address3") Address address3){
+        return new Person(name + " (from param)", age + 2, address3);
+    }
+
+    @Bean(name = "address")
+    public Address address(){
+        return new Address("123 Main St", "Riverdail");
+    }
+
+    @Bean(name = "address2")
+    public Address address2(){
+        return new Address("456 Oak Ave", "Springfield");
+    }
+
+    @Bean(name = "address3")
+    public Address address3(){
+        return new Address("789 Pine Rd", "Greendale");
+    }
+
+    @Bean
+    public String greeting(){
+        return "Hello from Spring Beans!";
     }
 
 }
