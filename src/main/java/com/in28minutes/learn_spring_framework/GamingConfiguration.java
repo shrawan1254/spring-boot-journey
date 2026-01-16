952146eb503830ca832917ca2308bb0e99d54b5f
shrawan dhawale <shrawan.dhawale@gmail.com>
2026-01-16 23:04:07 +0530

    Update Spring Framework project with gaming and helloworld apps

---
+++ src/main/java/com/in28minutes/learn_spring_framework/GamingConfiguration.java
@@ -0,0 +1,5 @@
+package com.in28minutes.learn_spring_framework;
+
+public class GamingConfiguration {
+
+}
---
+++ src/main/java/com/in28minutes/learn_spring_framework/helloworld/HelloWorlfConfiguration.java
@@ -0,0 +1,56 @@
+package com.in28minutes.learn_spring_framework.helloworld;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+record Person(String name, int age, Address address){};
+
+record Address(String street, String city){ };
+
+@Configuration
+public class HelloWorlfConfiguration {
+
+    @Bean
+    public String name(){
+        return "shrawan";
+    }
+
+    @Bean
+    public int age(){
+        return 21;
+    }   
+
+    @Bean
+    public Person person(){
+        return new Person("shrawan", 21, new Address("123 Main St", "Riverdail"));
+        
+    }
+    
+    @Bean
+    public Person person2MethodCall(){
+        return new Person(name(), age(), address());
+        
+    }
+
+      @Bean
+    public Person person3Parameter(String name, int age, Address address3){
+        return new Person(name, age, address3);
+        
+    }
+
+
+
+    @Bean(name = "address2")
+    public Address address(){
+        return new Address("123 Main St", "Riverdail");
+    }
+
+    @Bean(name = "address3")
+    public Address address(){
+        return new Address("123 Main St", "Riverdail");
+    }
+
+   
+
+}
