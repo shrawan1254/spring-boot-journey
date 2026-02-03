852929b626c172778efa6093e312c48d49b438d0
shrawan dhawale <shrawan.dhawale@gmail.com>
2026-02-03 20:37:42 +0530

    WIP: Before fixing App03GamingSpringBeans

--- src/main/java/com/in28minutes/learn_spring_framework/App03GamingSpringBeans.java
+++ src/main/java/com/in28minutes/learn_spring_framework/App03GamingSpringBeans.java
@@ -1,17 +1,29 @@
 package com.in28minutes.learn_spring_framework;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import com.in28minutes.learn_spring_framework.game.GameRunner;
 import com.in28minutes.learn_spring_framework.game.PacManGame;
 
-public class App01GamingBasicJava {
+public class App03GamingSpringBeans {
 
     public static void main(String[] args) {
 
+
+        var context = new AnnotationConfigApplicationContext(GamingConfiguration.class);
+
+        context.getBean(GamingConsole.class).up();
+
         //var game = new MarioGame();
-        //var gmae = new SuperContraGame();
-        var game = new PacManGame();
-        var gameRunner = new GameRunner(game);
-        gameRunner.run();
+        // //var gmae = new SuperContraGame();
+        // var game = new PacManGame();
+        // var gameRunner = new GameRunner(game);
+        // gameRunner.run();
+
+
+
+
+        
         
     }
 }
--- src/main/java/com/in28minutes/learn_spring_framework/GamingConfiguration.java
+++ src/main/java/com/in28minutes/learn_spring_framework/GamingConfiguration.java
@@ -1,5 +1,34 @@
 package com.in28minutes.learn_spring_framework;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.in28minutes.learn_spring_framework.game.GameConsole;
+import com.in28minutes.learn_spring_framework.game.PacManGame;
+
+
+@Configuration
 public class GamingConfiguration {
 
+
+    @Bean
+    public GamingConfiguration game() {
+        var game = new PacManGame();
+        return game;
+    }
+
+
+
+        //var game = new MarioGame();
+        //var gmae = new SuperContraGame();
+        //var game = new PacManGame();
+       // var gameRunner = new GameRunner(game);
+       // gameRunner.run();
+
+
+
+
+        
+        
+    
 }
