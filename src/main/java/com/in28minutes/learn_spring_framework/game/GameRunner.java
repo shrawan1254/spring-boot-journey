182baee899fadf3b3badd524f96db63c3e1470ea
shrawan dhawale <shrawan.dhawale@gmail.com>
2026-02-08 22:34:53 +0530

    Commit: update from assistant

--- src/main/java/com/in28minutes/learn_spring_framework/game/GameRunner.java
+++ src/main/java/com/in28minutes/learn_spring_framework/game/GameRunner.java
@@ -1,12 +1,14 @@
 package com.in28minutes.learn_spring_framework.game;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GameRunner {
     GamingConsole game;
 
-    public GameRunner(GamingConsole game) {
+    public GameRunner(@Qualifier("SuperContraGameQualifier") GamingConsole game) {
         this.game = game;
     }
 
