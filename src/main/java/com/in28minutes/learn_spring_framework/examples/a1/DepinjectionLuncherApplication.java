0ce6e3904623c51e8a13859cef69a4a6b6bcd984
shrawan dhawale <shrawan.dhawale@gmail.com>
2026-02-09 21:01:40 +0530

    Commit from Copilot

--- src/main/java/com/in28minutes/learn_spring_framework/examples/a1/DepinjectionLuncherApplication.java
+++ src/main/java/com/in28minutes/learn_spring_framework/examples/a1/DepinjectionLuncherApplication.java
@@ -3,10 +3,31 @@ package com.in28minutes.learn_spring_framework.examples.a1;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+
+@Component
+class YourBusinessClass{
+
+    @Autowired
+    Dependency1 dependency1;
+    @Autowired
+    Dependency2 dependency2;
+
+    public String toString(){
+        return "Using Dependency1: "+dependency1+" and Dependency2: "+dependency2;
+    }
+
+}@Component
+class Dependency1{
+
+}@Component
+class Dependency2{
+
+}
 
 @Configuration
 @ComponentScan
@@ -19,11 +40,8 @@ public class DepinjectionLuncherApplication {
             Arrays.stream(context.getBeanDefinitionNames())
             .forEach(System.out::println);
 
+            System.out.println(context.getBean(YourBusinessClass.class));
+
         }
-
-
-
-
-        
     }
 }
