3a8045207570fd599d9495ca549449d3906d657c
shrawan dhawale <shrawan.dhawale@gmail.com>
2026-02-10 16:20:27 +0530

    Update DepinjectionLuncherApplication

--- src/main/java/com/in28minutes/learn_spring_framework/examples/a1/DepinjectionLuncherApplication.java
+++ src/main/java/com/in28minutes/learn_spring_framework/examples/a1/DepinjectionLuncherApplication.java
@@ -12,10 +12,32 @@ import org.springframework.stereotype.Component;
 @Component
 class YourBusinessClass{
 
-    @Autowired
+   
     Dependency1 dependency1;
-    @Autowired
+   
     Dependency2 dependency2;
+
+    public YourBusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+        super();
+        System.out.println("Constructor Injection - YourBusinessClass");
+        this.dependency1 = dependency1;
+        this.dependency2 = dependency2;
+    }
+
+    // @Autowired
+    // public void setDependency1(Dependency1 dependency1) {
+    //     System.out.println("Setter Injection - setDependency1");
+    //     this.dependency1 = dependency1;
+    // }
+    // @Autowired
+    // public void setDependency2(Dependency2 dependency2) {
+    //     System.out.println("Setter Injection - setDependency2");
+    //     this.dependency2 = dependency2;
+    // }
+
+
+
+
 
     public String toString(){
         return "Using Dependency1: "+dependency1+" and Dependency2: "+dependency2;
